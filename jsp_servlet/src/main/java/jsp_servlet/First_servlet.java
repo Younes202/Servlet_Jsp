60560820bf686cac4b72a351625585bb9b991015
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-09-02 16:39:34 +0100

    doGet

--- jsp_servlet/src/main/java/jsp_servlet/First_servlet.java
+++ jsp_servlet/src/main/java/jsp_servlet/First_servlet.java
@@ -1,5 +1,7 @@
 package jsp_servlet;
 
-public class First_servlet extends HttpServlet {
+public class First_servlet extends HtpServlet {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	   this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
 
 }
