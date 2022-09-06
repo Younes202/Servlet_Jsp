4c1346ae8faa8f0ac06fc2792f72ca1970b6599e
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-09-06 16:53:15 +0100

    Create another servlet

--- jsp_servlet/src/main/java/jsp_servlet/welcome.java
+++ jsp_servlet/src/main/java/jsp_servlet/welcome.java
@@ -1,1 +1,10 @@
 
+package jsp_servlet;
+import jakarta.servlet.http.HttpServlet;
+
+public class welcome extends HtpServlet {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	  this.getServletContext().getRequestDispatcher("/WEB-INF/welcome.jsp").forward(req, resp);
+
+}
