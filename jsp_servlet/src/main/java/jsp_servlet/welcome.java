c1b1f3bacc635bc123183090344992f622b4b0ef
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-09-10 16:23:14 +0100

    create a array in servlet

--- jsp_servlet/src/main/java/jsp_servlet/welcome.java
+++ jsp_servlet/src/main/java/jsp_servlet/welcome.java
@@ -4,7 +4,8 @@ import jakarta.servlet.http.HttpServlet;
 
 public class welcome extends HtpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
+	 String []personnes = ["youens","james","muhammed"];
+	  req.setAtttribute("personnes",personnes);
 	  this.getServletContext().getRequestDispatcher("/WEB-INF/welcome.jsp").forward(req, resp);
 
 }
